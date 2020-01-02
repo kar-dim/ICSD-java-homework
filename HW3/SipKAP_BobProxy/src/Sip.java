2671db5b9d3d4969e5cb9f699787209562609c96
kar-dim <dim_kar@outlook.com>
2020-01-02 15:02:03 +0200

    Add files via upload

---
+++ HW3/SipKAP_BobProxy/src/Sip.java
@@ -0,0 +1,17 @@
+//Nikolaos Katsiopis icsd13076
+//Dimitrios Karatzas icsd13072
+
+public class Sip {
+    //request μηνύματα
+    public static String INVITE = "INVITE";
+    public static String ACK = "ACK";
+    public static String BYE = "BYE";
+    public static String REGISTER = "REGISTER";
+    
+    //response μηνύματα
+    public static String TRYING = "TRYING 100";
+    public static String RINGING = "RINGING 180";
+    public static String OK = "OK 200";
+    public static String BAD_REQUEST = "BAD REQUEST 400";
+    public static String UNAUTHORIZED = "UNAUTHORIZED 401";
+}
