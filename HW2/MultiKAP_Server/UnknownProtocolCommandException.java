24b605f0dba0857019bef9647d8e31cf62f1c99c
kar-dim <dim_kar@outlook.com>
2020-01-02 14:38:19 +0200

    Add files via upload

---
+++ HW2/MultiKAP_Server/UnknownProtocolCommandException.java
@@ -0,0 +1,9 @@
+//Nikolaos Katsiopis icsd13076
+//Dimitrios Karatzas icsd13072
+
+public class UnknownProtocolCommandException extends Exception {
+
+    public UnknownProtocolCommandException(String msg) {
+        super(msg);
+    }
+}
