3496e717eb86f15458a8164737c8ee451a322f32
Mimis <dim_kar@outlook.com>
2023-02-20 20:23:10 +0200

    delete HW1

--- HW1/sec3/PasswordTooShortException.java
+++
@@ -1,9 +0,0 @@
-package sec3;
-
-//μια απλή exception που εγείρεται όταν στο register ο χρήστης δώσει κωδικό <6 χαρακτήρες
-public class PasswordTooShortException extends Exception {
-    public PasswordTooShortException(String msg) {
-        super(msg);
-    }
-    public PasswordTooShortException(){}
-}
